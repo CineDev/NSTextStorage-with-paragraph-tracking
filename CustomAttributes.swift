c454f02a44b675eac4ecd1add65257ce6b8d1e76
CineDev <cinedevvv@gmail.com>
2018-02-04 06:16:06 +0200

    Add files via upload

---
+++ CustomAttributes.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTextAttributes.swift
+//  Core
+//
+//  Created by Vitaliy Vashchenko on 2/20/17.
+//  Copyright © 2017 Cine Studio. All rights reserved.
+//
+
+import Foundation
+
+public extension NSAttributedStringKey {
+	public static var identifier: NSAttributedStringKey {
+		return NSAttributedStringKey("identifier")
+	}
+}
