41e910bcdd608375b5c6478f04b01e3f7541434a
CineDev <cinedevvv@gmail.com>
2018-02-04 12:00:06 +0200

    Update String+Extensions.swift

--- String+Extensions.swift
+++ String+Extensions.swift
@@ -1,5 +1,10 @@
 public extension String {
-	/// Array of paragraphs of the string. Each paragraph except the last one ends with 'new line' character
+	/// Array of lines of the string. Line is a standalone string without 'new line' character
+	public var lines: [String] {
+		return self.components(separatedBy: .newlines)
+	}
+	
+	/// Array of paragraphs of the string. Each paragraph except the last one ends with the 'newline' character
 	public var paragraphs: [String] {
 		var paragraphs = self.lines
 		for i in 0 ..< paragraphs.count {
